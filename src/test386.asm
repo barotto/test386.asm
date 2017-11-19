3b22df307a80cb22027800210604007bb6576ad5
Marco Bortolin <barotto@gmail.com>
2017-11-19 18:45:48 +0100

    Add RCR,RCL undefined flags test
    Validated against 386SX.

--- src/test386.asm
+++ src/test386.asm
@@ -36,7 +36,7 @@
 ;   point on, all memory accesses should remain within the first 1MB.
 ;
 %define COPYRIGHT 'test386.asm (C) 2012-2015 Jeff Parsons, (C) 2017 Marco Bortolin      '
-%define RELEASE   '18/11/17'
+%define RELEASE   '19/11/17'
 
 	cpu 386
 	section .text
@@ -56,7 +56,7 @@ POST_PORT  equ 0x190 ; hex, the diagnostic port used to emit the current test pr
 LPT_PORT   equ 1     ; integer, parallel port to use, 0=LPT disabled, 1=3BCh, 2=378h, 3=278h
 COM_PORT   equ 0     ; integer, serial port to use, 0=COM disabled, 1=3F8h-3FDh, 2=2F8h-2FDh
 OUT_PORT   equ 0x0   ; hex, additional port for direct ASCII output, 0=disabled
-TEST_UNDEF equ 1     ; boolean, enable undefined behaviours tests
+TEST_UNDEF equ 0     ; boolean, enable undefined behaviours tests
 CPU_FAMILY equ 3     ; integer, used to test undefined behaviours, 3=80386
 IBM_PS1    equ 0     ; boolean, enable specific code for the IBM PS/1 2011 and 2121 models
 
@@ -867,7 +867,7 @@ shifts386FlagsTest:
 	testShiftWFlags   shl, 0x01, 32, 0,     0
 
 bt386FlagsTest:
-	; bt, btc, btr, bts
+	; BT, BTC, BTR, BTS
 	; undefined flags:
 	;  OF: same as RCR with CF=0
 	testBittestFlags   0x01, 0, 0,     PS_CF
@@ -878,6 +878,28 @@ bt386FlagsTest:
 	testBittestFlags   0x01, 2, PS_CF, PS_OF
 	testBittestFlags   0x01, 3, 0,     0
 	testBittestFlags   0x01, 3, PS_CF, 0
+
+rotate386FlagsTest:
+	; RCR
+	; CF and OF are set with byte and count=9 or word and count=17
+	testShiftBFlags   rcr, 0,    9, 0,           0
+	testShiftBFlags   rcr, 0,    9, PS_CF|PS_OF, PS_CF
+	testShiftBFlags   rcr, 0x40, 9, 0,           PS_OF
+	testShiftBFlags   rcr, 0x40, 9, PS_CF|PS_OF, PS_CF|PS_OF
+	testShiftWFlags   rcr, 0,      17, 0,           0
+	testShiftWFlags   rcr, 0,      17, PS_CF|PS_OF, PS_CF
+	testShiftWFlags   rcr, 0x4000, 17, 0,           PS_OF
+	testShiftWFlags   rcr, 0x4000, 17, PS_CF|PS_OF, PS_CF|PS_OF
+	; RCL
+	; CF and OF are set with byte and count=9 or word and count=17
+	testShiftBFlags   rcl, 0,    9, 0,           0
+	testShiftBFlags   rcl, 0,    9, PS_CF|PS_OF, PS_CF|PS_OF
+	testShiftBFlags   rcl, 0x80, 9, 0,           PS_OF
+	testShiftBFlags   rcl, 0x80, 9, PS_CF|PS_OF, PS_CF
+	testShiftWFlags   rcl, 0,      17, 0,           0
+	testShiftWFlags   rcl, 0,      17, PS_CF|PS_OF, PS_CF|PS_OF
+	testShiftWFlags   rcl, 0x8000, 17, 0,           PS_OF
+	testShiftWFlags   rcl, 0x8000, 17, PS_CF|PS_OF, PS_CF
 
 	jmp arithLogicTests
 
